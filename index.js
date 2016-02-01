736c42fc93f74c7333abe9141d30c3e356d24d0a
surunzi <surunzi@foxmail.com>
2016-02-01 19:15:28 +0800

    Dev: Small changes

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 var cheerio = require('cheerio'),
     _ = require('./lib/util');
 
-module.exports = function (src)
+module.exports = function (options)
 {
-    var ret = '';
-
-    src = src.replace(/<template\s+id="(\w+)">/g, '<script id="$1" type="text/template">');
-    src = src.replace(/<\/template>/g, '</script>');
-
-    var $ = cheerio.load(src);
-
-    $.root().children().each(function ()
+    return function (src)
     {
-        var $this = $(this);
+        var ret = '';
 
-        if (this.tagName === 'require') return ret += '_(\'' + _.trim($this.text()) + '\');\n';
+        src = src.replace(/<template\s+id="(\w+)">/g, '<script id="$1" type="text/template">');
+        src = src.replace(/<\/template>/g, '</script>');
 
-        if (this.tagName === 'script')
+        var $ = cheerio.load(src);
+
+        $.root().children().each(function ()
         {
-            if ($this.attr('type') === 'text/template')
+            var $this = $(this);
+
+            if (this.tagName === 'require') return ret += '_(\'' + _.trim($this.text()) + '\');\n';
+
+            if (this.tagName === 'script')
             {
-                return ret += 'var ' + $this.attr('id') + ' = \'' +
-                              _.jsEncode($this.text()) + '\';\n';
+                if ($this.attr('type') === 'text/template')
+                {
+                    return ret += 'var ' + $this.attr('id') + ' = \'' +
+                        _.jsEncode($this.text()) + '\';\n';
+                }
+
+                return ret += _.trim($this.text());
             }
+        });
 
-            return ret += _.trim($this.text());
-        }
-    });
-
-    return ret;
+        return ret;
+    };
 };
